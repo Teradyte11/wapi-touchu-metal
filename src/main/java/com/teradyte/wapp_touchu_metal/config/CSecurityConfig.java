88e8052963363e455567384ff3b53bbe0464ca3e
javier.vivero.ramos <x.vivero.ramoz@gmail.com>
2025-12-26 21:56:49 -0500

    feat:initial configuration for security configuration

---
+++ src/main/java/com/teradyte/wapp_touchu_metal/config/CSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.teradyte.wapp_touchu_metal.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class CSecurityConfig {
+    /**
+     * Method for cors configuration.
+     * 
+     * @return
+     */
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
